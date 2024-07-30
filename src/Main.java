bcb6aa43c8c6ed67fbc564181b1b5d16278319ba
unknown <javier_j_medina@hotmail.com>
2024-07-30 02:03:23 -0300

    add first exercise

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Articulo articulo = new Articulo(1, "Laptop", 999.99f, 10);
         articulo.mostrarArticulo();
+        Socio socio1 = new Socio("Medina");
+        socio1.mostrarSocio();
+        Socio socio2 = new Socio("Javier");
+        socio2.mostrarSocio();
+
+        Administrativo ejemplo1 = Administrativo.ejemplo1();
+        Administrativo ejemplo2 = Administrativo.ejemplo1();
+        if (ejemplo1.getSueldo() > ejemplo2.getSueldo()) {
+            System.out.println(ejemplo1.getNombre() + " gana más que " + ejemplo2.getNombre());
+        } else if (ejemplo1.getSueldo() < ejemplo2.getSueldo()) {
+            System.out.println(ejemplo2.getNombre() + " gana más que " + ejemplo1.getNombre());
+        } else {
+            System.out.println(ejemplo1.getNombre() + " y " + ejemplo2.getNombre() + " ganan lo mismo.");
+        }
     }
 } 
---
+++ src/Socio.java
@@ -0,0 +1,28 @@
+public class Socio {
+    private String nombre;
+    private int numero;
+    private static int proximoNumero = 1;
+
+    public Socio (String nombre){
+        this.nombre = nombre;
+        numero = proximoNumero;
+        proximoNumero++;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    //no es necesario el setNumero porque el numero se asigna automaticamente
+//    public void setNumero(int numero){
+//        this.numero = numero;
+//    }
+    public void mostrarSocio(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Numero: " + numero);
+    }
+}
