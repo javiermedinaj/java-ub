a444edc876a3b980613f9d43f37d455a0285991f
medinajavierj <javier_j_medina@hotmail.com>
2024-09-02 22:33:23 -0300

    add files

---
+++ src/BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+    private long dni;
+    private long accountNumber;
+    private double saldo;
+    private double interestRate;
+
+    //constructor
+    public BankAccount(){
+        this.accountNumber = 
+    }
+
+
+
+}
